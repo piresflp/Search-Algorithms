c11aa92886da059d74b13f6979dedd76abd02ea5
Felipe Pires <48968398+piresflp@users.noreply.github.com>
2020-09-26 20:08:45 -0300

    init + classes Arvore e NoArvore

---
+++ apCaminhosMarte/Arvore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    class Arvore<Dado> where Dado : IComparable<Dado>
+    {
+        private NoArvore<Dado> raiz, atual, antecessor;
+
+        public NoArvore<Dado> Raiz { get => raiz; set => raiz = value; }
+        public NoArvore<Dado> Atual { get => atual; set => atual = value; }
+        public NoArvore<Dado> Antecessor { get => antecessor; set => antecessor = value; }
+    }
+}
---
+++ apCaminhosMarte/NoArvore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    class NoArvore<Dado> : IComparable<NoArvore<Dado>> where Dado : IComparable<Dado>
+    {
+        Dado info;
+        NoArvore<Dado> esq;
+        NoArvore<Dado> dir;
+        int altura;
+
+        public NoArvore(Dado Informacao)
+        {
+            this.Info = Informacao;
+            this.esq = null;
+            this.dir = null;
+        }
+
+        public NoArvore(Dado dados, NoArvore<Dado> esquerdo, NoArvore<Dado> direito)
+        {
+            this.Info = dados;
+            this.Esq = esquerdo;
+            this.Dir = direito;
+        }
+
+        public Dado Info { get => info; set => info = value; }
+        public NoArvore<Dado> Esq { get => esq; set => esq = value; }
+        public NoArvore<Dado> Dir { get => dir; set => dir = value; }
+
+        public int CompareTo(NoArvore<Dado> o)
+        {
+            return info.CompareTo(o.info);
+        }
+
+        public bool Equals(NoArvore<Dado> o)
+        {
+            return this.info.Equals(o.info);
+        }
+    }
+}
